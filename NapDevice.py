cd7388d135df32c03a0904551663a58cdd6be332
Tiago Coutinho <coutinho@bcu01ctrl.esrf.fr>
2014-08-22 08:58:36 +0200

    Replace tab with spaces

--- NapDevice.py
+++ NapDevice.py
@@ -5,19 +5,22 @@
 from PyTango.server import Device, DeviceMeta, run
 from PyTango.server import command
 import time
-        
+
 import tangoworker
-        
+
+
 def nap(nap_time, msg="Finished sleeping"):
-	time.sleep(nap_time)
-	print(msg)
-        
+    time.sleep(nap_time)
+    print(msg)
+
+
 class NapDevice(Device):
-	__metaclass__ = DeviceMeta
-        
-	@command(dtype_in=float)
-	def nap(self, nap_time):
-		tangoworker.execute(nap, nap_time, msg="I am finished!")
-        
+    __metaclass__ = DeviceMeta
+
+    @command(dtype_in=float)
+    def nap(self, nap_time):
+        tangoworker.execute(nap, nap_time, msg="I am finished!")
+
+
 run([NapDevice])
 
